d190326560260c16fb52aef8910c475199c8a341
null <khf.13.14.zz@gmail.com>
2026-04-02 23:24:35 +0900

    Rearrage tmux config directory

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -3,6 +3,6 @@
   # Requirements
   # git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
   programs.tmux.enable = true;
-  xdg.configFile.tmux.source = ./tmux;
-  xdg.configFile.tmux-powerline.source = ./tmux-powerline;
+  xdg.configFile.tmux.source = ./config/tmux;
+  xdg.configFile.tmux-powerline.source = ./config/tmux-powerline;
 }
