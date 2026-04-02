feb41887ecaca4dd3cd9ef26a0eb5275db498179
null <khf.13.14.zz@gmail.com>
2026-04-02 23:08:48 +0900

    Specify tmux-powerline configfile source path

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -4,4 +4,5 @@
   # git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
   programs.tmux.enable = true;
   xdg.configFile.tmux.source = ./tmux;
+  xdg.configFile.tmux-powerline.source = ./tmux-powerline;
 }
