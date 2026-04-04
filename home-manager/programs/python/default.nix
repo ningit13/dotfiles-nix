91902a67864fc1ccfea8fa69ab031675ac85176e
null <khf.13.14.zz@gmail.com>
2026-04-05 05:25:33 +0900

    fix tmux plugins configuration

--- home-manager/programs/python/default.nix
+++ home-manager/programs/python/default.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ python314 ];
+  home.packages = with pkgs; [
+    (
+      python314.withPackages (ps: with ps; [
+        libtmux # required for tmux-window-name (plugin)
+        pynvim  # required for treemux (plugin)
+      ])
+    )
+  ];
 }
--- home-manager/programs/tmux/plugins.nix
+++ home-manager/programs/tmux/plugins.nix
@@ -4,24 +4,41 @@ let
     tmuxPlugins
     fetchFromGitHub
     ;
+
+  treemux = tmuxPlugins.mkTmuxPlugin {
+    pluginName = "treemux";
+    version = "";
+    rtpFilePath = "sidebar.tmux";
+    src = fetchFromGitHub {
+      owner = "kiyoon";
+      repo = "treemux";
+      rev = "master";
+      sha256 = "sha256-1mCxTv3KqUsCjeI7X02NBMRJJzbL0cE1Gg20FrMDChI=";
+    };
+  };
 in
 with pkgs.tmuxPlugins;
 [
-  tmux-powerline
+  sensible
   {
     plugin = tmuxPlugins.mkTmuxPlugin {
-      pluginName = "treemux";
-      version = "";
+      pluginName = "powerline";
+      version = "3.2.0";
+      rtpFilePath = "main.tmux";
       src = fetchFromGitHub {
-        owner = "kiyoon";
-        repo = "treemux";
-        rev = "master";
-        sha256 = "sha256-1mCxTv3KqUsCjeI7X02NBMRJJzbL0cE1Gg20FrMDChI=";
+        owner = "erikw";
+        repo = "tmux-powerline";
+        rev = "6079ace8d534a01d4d964b8b854b223f72edaf4b";
+        hash = "sha256-0ibtd1gTyr8hJDBsAfmgH3qr0zC0o2Fn0tjN/S+zxgA=";
       };
     };
+  }
+  {
+    plugin = treemux;
     extraConfig = ''
+      set -g @treemux-python-command '${pkgs.python3}/bin/python3'
       set -g @treemux-tree-client 'neo-tree'  # or 'nvim-tree'
-      # set -g @treemux-tree-nvim-init-file '~/.local/share/tmux/plugins/treemux/configs/treemux_init.lua'
+      set -g @treemux-tree-nvim-init-file '${treemux}/share/tmux-plugins/treemux/configs/treemux_init.lua'
 
       # If you want to completely isolate the treemux's nvim config and data from your main nvim config and data, set NVIM_APPNAME.
       # This will make treemux use '~/.config/nvim-treemux', '~/.local/share/nvim-treemux'
@@ -32,6 +49,7 @@ with pkgs.tmuxPlugins;
     plugin = tmuxPlugins.mkTmuxPlugin {
       pluginName = "tmux-window-name";
       version = "";
+      rtpFilePath = "tmux_window_name.tmux";
       src = pkgs.fetchFromGitHub {
         owner = "ofirgall";
         repo = "tmux-window-name";
