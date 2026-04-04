1a9dbb5ace53755add0fdeda1d47f00250fd275f
null <khf.13.14.zz@gmail.com>
2026-04-05 02:04:59 +0900

    Split xdg configurations into nix file

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -4,15 +4,6 @@
   ...
 }:
 {
-  xdg = {
-    enable = true;
-
-    configHome = "${config.home.homeDirectory}/.config";
-    dataHome = "${config.home.homeDirectory}/.local/share";
-    stateHome = "${config.home.homeDirectory}/.local/state";
-    cacheHome = "${config.home.homeDirectory}/.cache";
-  };
-
   home = {
     stateVersion = "25.11";
 
---
+++ home-manager/misc/xdg/default.nix
@@ -0,0 +1,11 @@
+{ config, ... }:
+{
+  xdg = {
+    enable = true;
+
+    configHome = "${config.home.homeDirectory}/.config";
+    dataHome = "${config.home.homeDirectory}/.local/share";
+    stateHome = "${config.home.homeDirectory}/.local/state";
+    cacheHome = "${config.home.homeDirectory}/.cache";
+  };
+}
