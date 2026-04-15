18f58697fdd3311a84c4cffceb72f60cab536777
ningit <ningit13@pm.me>
2026-04-16 00:06:59 +0900

    chore(work): clear git credentials from work host profile

--- hosts/work/profile.nix
+++ hosts/work/profile.nix
@@ -3,6 +3,6 @@ rec {
   homeDirectory = "/home/${username}";
   system = "x86_64-linux";
 
-  gitUserName = "kohei_fujiwara_gst";
-  gitUserEmail = "kohei_fujiwara_gst@jp.honda";
+  gitUserName = "";
+  gitUserEmail = "";
 }
