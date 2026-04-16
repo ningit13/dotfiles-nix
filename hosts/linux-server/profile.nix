554113bf612f95b498955782deef77d46334d152
ningit13 <ningit13@pm.me>
2026-04-17 00:42:43 +0900

    fix(git): correct git username to ningit13 across host profiles

--- hosts/linux-server/profile.nix
+++ hosts/linux-server/profile.nix
@@ -3,7 +3,7 @@ rec {
   homeDirectory = "/home/${username}";
   system = "x86_64-linux";
 
-  gitUserName = "ningit";
+  gitUserName = "ningit13";
   gitUserEmail = "ningit13@pm.me";
   gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+r3PKmqWo7dZ7sqhdMJ+19Kzn3YDV6zdYIbB7BGGhR";
 }
--- hosts/mac/profile.nix
+++ hosts/mac/profile.nix
@@ -3,7 +3,7 @@ rec {
   homeDirectory = "/Users/${username}";
   system = "aarch64-darwin";
 
-  gitUserName = "ningit";
+  gitUserName = "ningit13";
   gitUserEmail = "ningit13@pm.me";
   gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+r3PKmqWo7dZ7sqhdMJ+19Kzn3YDV6zdYIbB7BGGhR";
 }
