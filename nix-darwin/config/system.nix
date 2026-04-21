ea3873b26ce5c055f37b65a1460c868ea039dad8
ningit13 <ningit13@pm.me>
2026-04-21 22:42:10 +0900

    chore(nix-darwin): add dark mode, dock, and auto-update defaults

--- nix-darwin/config/system.nix
+++ nix-darwin/config/system.nix
@@ -1,17 +1,23 @@
-{ profile, config, ... }:
+{ profile }:
 {
   system = {
     stateVersion = 6;
     primaryUser = profile.username;
 
     defaults = {
+      SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+
       NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleInterfaceStyleSwitchesAutomatically = false;
         "com.apple.swipescrolldirection" = false;
       };
 
       dock = {
         launchanim = false;
         autohide = true;
+        show-recents = false;
+        orientation = "bottom";
       };
     };
   };
