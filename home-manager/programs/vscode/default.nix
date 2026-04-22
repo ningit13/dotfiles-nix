f8fe91bc9b7870830d399ab0056ab2bfd5a283c8
ningit13 <ningit13@pm.me>
2026-04-23 00:50:07 +0900

    feat(vscode): add editor settings for font and Claude Code panel location

--- home-manager/programs/vscode/default.nix
+++ home-manager/programs/vscode/default.nix
@@ -12,17 +12,29 @@ in
 {
   programs.vscode = {
     enable = true;
-    profiles.default.extensions =
-      (with pkgs.vscode-extensions; [
-        ms-vscode.remote-explorer
-        ms-vscode-remote.remote-ssh
-        ms-vscode-remote.remote-ssh-edit
-        ms-vscode-remote.remote-containers
-        ms-azuretools.vscode-containers
-        editorconfig.editorconfig
-        jnoortheen.nix-ide
-        github.vscode-github-actions
-      ])
-      ++ [ claude-code ];
+    profiles.default = {
+      extensions =
+        (with pkgs.vscode-extensions; [
+          ms-vscode.remote-explorer
+          ms-vscode-remote.remote-ssh
+          ms-vscode-remote.remote-ssh-edit
+          ms-vscode-remote.remote-containers
+          ms-azuretools.vscode-containers
+          editorconfig.editorconfig
+          jnoortheen.nix-ide
+          github.vscode-github-actions
+        ])
+        ++ [ claude-code ];
+
+      userSettings = {
+        "editor.fontFamily" = "'CaskaydiaMono Nerd Font Mono'";
+        "editor.minimap.enabled" = false;
+        "editor.renderWhitespace" = "boundary";
+
+        "terminal.integrated.fontFamily" = "'CaskaydiaMono Nerd Font Mono'";
+
+        "claudeCode.preferredLocation" = "panel";
+      };
+    };
   };
 }
