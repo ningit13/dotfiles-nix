e385e7ebb745e9e541b1ee7597cc26a4dcb2d385
ningit13 <ningit13@pm.me>
2026-06-03 23:34:44 +0900

    style(nix-darwin): annotate brew casks managed by home-manager

--- nix-darwin/config/environment.nix
+++ nix-darwin/config/environment.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    brewCasks.brave-browser
-    brewCasks.bitwarden
-    brewCasks.visual-studio-code
+    brewCasks.brave-browser # configured at home-manager level
+    brewCasks.bitwarden # configured at home-manager level
+    brewCasks.visual-studio-code # configured at home-manager level
     brewCasks.rectangle
-    brewCasks.obsidian
+    brewCasks.obsidian # configured at home-manager level
     brewCasks.claude
     brewCasks.proton-mail
     brewCasks.slack
