8aa8eec0f24a8f038a0ed3044bab3c26c4dbee3b
ningit13 <ningit13@pm.me>
2026-06-03 23:34:35 +0900

    refactor(work): update import paths to use new program category structure

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -22,18 +22,18 @@ home-manager.lib.homeManagerConfiguration {
     nixvim.homeModules.nixvim
     ../../nix
     ../../home-manager/misc
-    ../../home-manager/programs/direnv
-    ../../home-manager/programs/eza
-    ../../home-manager/programs/fd
-    ../../home-manager/programs/fzf
-    ../../home-manager/programs/jq
-    ../../home-manager/programs/lazygit
-    ../../home-manager/programs/python
-    ../../home-manager/programs/ripgrep
-    ../../home-manager/programs/tmux
-    ../../home-manager/programs/yazi
-    ../../home-manager/programs/zoxide
-    ../../home-manager/programs/zsh
+    ../../home-manager/programs/editor
+    ../../home-manager/programs/lang
+    ../../home-manager/programs/terminal
+    ../../home-manager/programs/tools/direnv
+    ../../home-manager/programs/tools/eza
+    ../../home-manager/programs/tools/fd
+    ../../home-manager/programs/tools/fzf
+    ../../home-manager/programs/tools/jq
+    ../../home-manager/programs/tools/lazygit
+    ../../home-manager/programs/tools/ripgrep
+    ../../home-manager/programs/tools/yazi
+    ../../home-manager/programs/tools/zoxide
     ../../home-manager/services
     {
       home.homeDirectory = homeDirectory;
