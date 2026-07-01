666ec2ea4be439018b4a91fa98027ceff132c872
ningit13 <ningit13@pm.me>
2026-07-02 00:38:35 +0900

    feat(nixvim): show dotfiles in neo-tree

--- home-manager/programs/editor/nixvim/plugins/neo-tree/default.nix
+++ home-manager/programs/editor/nixvim/plugins/neo-tree/default.nix
@@ -2,6 +2,13 @@
   plugins = {
     neo-tree = {
       enable = true;
+      settings = {
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false;
+          };
+        };
+      };
     };
     # required for neo-tree (optional)
     web-devicons = {
