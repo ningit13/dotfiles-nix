142305b93692419930ae3e26058985661f453f09
ningit13 <ningit13@pm.me>
2026-07-02 17:49:05 +0900

    feat(yazi): show hidden files, sort dirs first, use natural sort

--- home-manager/programs/cli-tools/yazi/default.nix
+++ home-manager/programs/cli-tools/yazi/default.nix
@@ -3,5 +3,12 @@
     enable = true;
     enableZshIntegration = true;
     shellWrapperName = "yy";
+    settings = {
+      mgr = {
+        show_hidden = true;
+        sort_dir_first = true;
+        sort_by = "natural";
+      };
+    };
   };
 }
