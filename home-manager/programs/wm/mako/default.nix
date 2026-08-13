63172ae5050f0bfe98d97f5c5fd35306a9cab987
ningit13 <ningit13@pm.me>
2026-08-14 02:20:18 +0900

    feat(mako): style notifications and add urgency-based theming
    Sets font, colors, borders, and per-urgency timeouts/colors for
    notification popups.

--- home-manager/programs/wm/mako/default.nix
+++ home-manager/programs/wm/mako/default.nix
@@ -1,3 +1,38 @@
 {
   services.mako.enable = true;
+  services.mako.settings = {
+    # Global options
+    font = "CaskaydiaCove Nerd Font Propo 12";
+    background-color = "#1b2023ee";
+    text-color = "#acb0d0";
+    progress-color = "over #3cae34ee";
+    width = 300;
+    height = 100;
+    default-timeout = 5000;
+    layer = "overlay";
+
+    # - border options
+    border-color = "#f9c993ee";
+    border-size = 1;
+    border-radius = 8;
+
+    # - icon options
+    icons = true;
+    max-icon-size = 36;
+    icon-location = "left";
+    icon-border-radius = 0;
+
+    "urgency=low" = {
+      default-timeout = 3000;
+      border-color = "#54ff44ee";
+    };
+    "urgency=normal" = {
+      default-timeout = 5000;
+      border-color = "#46ceffee";
+    };
+    "urgency=critical" = {
+      default-timeout = 0;
+      border-color = "#ff4646ee";
+    };
+  };
 }
